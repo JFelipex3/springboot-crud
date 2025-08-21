218323d7debd050f1dbd97bedde041b5720f5c9a
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-08-21 13:10:57 -0400

    Conexión a Base de Datos según convención

--- src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
+++ src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
@@ -3,17 +3,12 @@ package com.jmachuca.curso.springboot.app.springboot_crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class SpringbootCrudApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(SpringbootCrudApplication.class, args);
+		System.out.println("Spring Boot CRUD Application is running!");
 	}
 
 }
