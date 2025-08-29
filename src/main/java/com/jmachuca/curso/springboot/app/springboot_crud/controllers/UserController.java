b8ce1603d2690440404dbabfbe2f70b98993e7cd
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-08-29 19:15:57 -0400

    Implementar el controller

---
+++ src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.jmachuca.curso.springboot.app.springboot_crud.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jmachuca.curso.springboot.app.springboot_crud.entities.User;
+import com.jmachuca.curso.springboot.app.springboot_crud.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> list() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result){
+
+        if(result.hasFieldErrors()){
+            return validation(result);
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
+    }
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
