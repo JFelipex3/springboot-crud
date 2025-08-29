da3c4bffd48c9ccbed1cac112eee8a7ce6293bf3
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-08-29 09:38:16 -0400

    Componente service, Repository y Config SpringSecurity

--- src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/entities/User.java
+++ src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/entities/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
 
 @Entity
@@ -34,6 +35,9 @@ public class User {
         uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role_id"})}
     )
     private List<Role> roles;
+
+    @Transient
+    private boolean admin;
 
     public Long getId() {
         return id;
@@ -65,6 +69,14 @@ public class User {
 
     public void setRoles(List<Role> roles) {
         this.roles = roles;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
     }
 
 }
---
+++ src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/services/UserService.java
@@ -0,0 +1,12 @@
+package com.jmachuca.curso.springboot.app.springboot_crud.services;
+
+import java.util.List;
+
+import com.jmachuca.curso.springboot.app.springboot_crud.entities.User;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User save(User user);
+}
