45164fa35dd9f074ec011ab5fe07f4bc524b1e8b
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-09-01 12:42:05 -0400

    Se implemeta successfulAuthentication y se genera llave firmando el token JWT, se agregan roles en los clamins cuando se genera token y se implenta clase de Constantes

--- src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.jmachuca.curso.springboot.app.springboot_crud.security.filter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
@@ -13,8 +18,14 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jmachuca.curso.springboot.app.springboot_crud.entities.User;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import static com.jmachuca.curso.springboot.app.springboot_crud.utils.AppConstants.*;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
@@ -35,7 +46,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             user = new ObjectMapper().readValue(request.getInputStream(), User.class);
             username = user.getUsername();
             password = user.getPassword();
-            
+
         } catch (StreamReadException e) {
             e.printStackTrace();
         } catch (DatabindException e) {
@@ -48,5 +59,37 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         return authenticationManager.authenticate(authenticationToken);
     }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        
+        User user = (User) authResult.getPrincipal();
+        String username = user.getUsername();
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+        Claims claims = Jwts.claims().build();
+        claims.put("authorities", roles);
+
+        String token = Jwts.builder()
+            .subject(username)
+            .expiration(new Date(System.currentTimeMillis() + 3600000))
+            .issuedAt(new Date())
+            .signWith(SECRET_KEY)
+            .compact();
+
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_BEARER + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put(TOKEN, token);
+        body.put(USERNAME, username);
+        body.put(MESSAGE, String.format("Hola %s has iniciado sesion con exito!", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(ContentType.JSON.getValue());
+        response.setStatus(200);
+        
+    }
+
+    
 
 }
---
+++ src/main/java/com/jmachuca/curso/springboot/app/springboot_crud/utils/AppConstants.java
@@ -0,0 +1,42 @@
+package com.jmachuca.curso.springboot.app.springboot_crud.utils;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Jwts;
+
+public class AppConstants {
+
+    public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String PREFIX_BEARER = "Bearer ";
+    public static final String TOKEN = "token";
+    public static final String USERNAME = "username";
+    public static final String MESSAGE = "message";
+    
+    public enum ContentType {
+        JSON("application/json"),
+        XML("application/xml"),
+        HTML("text/html"),
+        TEXT("text/plain"),
+        JAVASCRIPT("application/javascript"),
+        CSS("text/css"),
+        FORM_URLENCODED("application/x-www-form-urlencoded"),
+        MULTIPART("multipart/form-data"),
+        CSV("text/csv");
+
+        private final String value;
+
+        ContentType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+}
